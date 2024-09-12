7c30b9a9584b4e15c409037409df117a1810afdd
tgazzaev <tgazzaev@samokat.ru>
2024-09-12 14:31:10 +0300

    fix

--- src/services/wg.ts
+++ src/services/wg.ts
@@ -13,7 +13,7 @@ const profilePath = `/etc/wireguard/${wgParams.SERVER_WG_NIC}.conf`
 const exec = async (command: string) => {
     try {
         const { stdout, stderr } = await execute(command)
-        if (stderr) throw new Error(stderr)
+        if (stderr) console.error(stderr)
 
         return stdout
       } catch (e) {
